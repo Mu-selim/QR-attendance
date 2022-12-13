e95abcc3cca69e9f4e6449adeab72349a91282ec
selim <muhamadselim70@gmail.com>
2022-12-13 22:47:05 +0200

    copy text update

--- app/src/main/java/com/devselim/qrattendace/AttendaceActivity.java
+++ app/src/main/java/com/devselim/qrattendace/AttendaceActivity.java
@@ -2,8 +2,14 @@ package com.devselim.qrattendace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.devselim.qrattendace.databinding.ActivityAttendaceBinding;
 
@@ -22,5 +28,21 @@ public class AttendaceActivity extends AppCompatActivity {
         ArrayList<String> sent = bundle.getStringArrayList("array_sent");
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sent);
         binding.attendaceBox.setAdapter(arrayAdapter);
+
+        binding.attendaceBox.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                copyText(sent.get(i));
+                return false;
+            }
+        });
+    }
+
+    private void copyText(String text) {
+        ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("AttendanceItem", text);
+        manager.setPrimaryClip(clipData);
+
+        Toast.makeText(AttendaceActivity.this, "Copied to clipboard", Toast.LENGTH_LONG).show();
     }
 }
