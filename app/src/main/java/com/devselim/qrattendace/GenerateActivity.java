5f44a770685158ca85b8831e29a80d7fb7a8eb71
selim <muhamadselim70@gmail.com>
2022-12-13 23:40:33 +0200

    v1.0.2 update

--- app/src/main/java/com/devselim/qrattendace/GenerateActivity.java
+++ app/src/main/java/com/devselim/qrattendace/GenerateActivity.java
@@ -1,11 +1,30 @@
 package com.devselim.qrattendace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.devselim.qrattendace.databinding.ActivityGenerateBinding;
+
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.util.Objects;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
@@ -13,6 +32,7 @@ import androidmads.library.qrgenearator.QRGEncoder;
 public class GenerateActivity extends AppCompatActivity {
 
     ActivityGenerateBinding binding;
+    private static final int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +47,61 @@ public class GenerateActivity extends AppCompatActivity {
                 binding.generatedQRImage.setImageBitmap(encoder.getBitmap());
             }
         });
+
+        binding.downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(GenerateActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    saveImage();
+                } else {
+                    ActivityCompat.requestPermissions(GenerateActivity.this, new String[] {
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    }, REQUEST_CODE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImage();
+            } else {
+                Toast.makeText(GenerateActivity.this, "Please provide required permissions", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void saveImage() {
+        Uri images;
+        ContentResolver contentResolver = getContentResolver();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            images = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        } else {
+            images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, System.currentTimeMillis()+".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "images/*");
+        Uri uri = contentResolver.insert(images, contentValues);
+
+        try {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) binding.generatedQRImage.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(Objects.requireNonNull(uri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            Objects.requireNonNull(outputStream);
+
+            Toast.makeText(GenerateActivity.this, "Saved successfully", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(GenerateActivity.this, "Image failed to be saved.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
